0b24d5fdee276a44489291dbc71b73bc66e4326a
jan-molak <jan.molak@gmail.com>
2013-01-09 16:30:36 +0000

    Extracted tomcat into a separate module

--- puppet/modules/tomcat/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-class tomcat(
-  $version = '6'
-) {
-  case $version {
-    6: { include tomcat::tomcat6 }
-    default: { fail("This class only knows how to handle tomcat 6 I'm afraid :(") }
-  }
-}
